2d32ab5ec9eee9003b25338f756d3dbb61a7e138
Oxana2332 <far_north@ro.ru>
2022-01-24 19:39:58 +0300

    убрала return false в каждом ветвлении и добавила правильные атрибуты inputам

--- registration.js
+++ registration.js
@@ -12,22 +12,16 @@ function check() {
 
     if (fname.value.length <= 0) {
         document.getElementById('error').innerHTML += "Пожалуйста, введите имя <br>";
-        return false;
     } else if (lname.value.length <= 0) {
         document.getElementById('error').innerHTML += "Пожалуйста, введите фамилию <br>";
-        return false;
     } else if (password.value.length <= 7) {
         document.getElementById('error').innerHTML += "Необходимо ввести пароль, состоящий не менее, чем из 8 символов <br>";
-        return false;
     } else if ((!email.value.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/))) {
         document.getElementById('error').innerHTML += "Необходимо ввести емейл<br>";
-        return false;
     } else if (!(tel.value.match(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/))) {
         document.getElementById('error').innerHTML += "Необходимо ввести телефон<br>";
-        return false;
     } else if (!(chbox.checked)) {
         alert('Для завершения регистрации примите условия Пользовательского соглашения и дайте согласие на обработку персональных данных');
-        return false;
     } else {
         alert('Добро пожаловать, ' + fname.value + ' ' + lname.value + '!');
     }
